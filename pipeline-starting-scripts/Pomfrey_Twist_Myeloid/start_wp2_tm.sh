cd6f3f9f3b5876a3dc53c8a419c84e573dad18ae
Arielle R Munters <arielle@reivant.se>
2023-11-09 15:47:09 +0100

    fix: add smallscript to git clone

--- pipeline-starting-scripts/Pomfrey_Twist_Myeloid/start_wp2_tm.sh
+++ pipeline-starting-scripts/Pomfrey_Twist_Myeloid/start_wp2_tm.sh
@@ -3,13 +3,14 @@
 set -e
 # cwd is scratch
 git_repo_url_tm="https://github.com/clinical-genomics-uppsala/pomfrey"
+git_repo_url_smallscripts="https://github.com/clinical-genomics-uppsala/WP2_smallscripts.git"
 
 # Initialize variables
 inbox_path=""
 analysis_path=""
 pipeline_version=""
+smallscripts_version=""
 sequenceid=""
-config="" #"/projects/wp2/nobackup/WP2_smallscripts/pipeline-starting-scripts/Pomfrey_Twist_myeloid/config_defaults_230412.yaml"
 samplesheet=""
 
 # Process options and arguments
@@ -23,16 +24,16 @@ while[[ $# -gt 0 ]]; do
         analysis_path="$2"
         shift 2
         ;;
-    --pipeline_version)
+    --pipeline-version)
         pipeline_version="$2"
         shift 2 
         ;;
-    --sequenceid)
-        sequenceid="$2"
+    --smallscripts-version)
+        smallscripts_version="$2"
         shift 2
         ;;
-    --config)
-        config="$2"
+    --sequenceid)
+        sequenceid="$2"
         shift 2
         ;;
     --samplesheet)
@@ -58,17 +59,17 @@ if [ -z "$analysis_path" ]; then
 fi
 
 if [ -z "$pipeline_version" ]; then
-  echo "Error: --pipeline_version is required."
+  echo "Error: --pipeline-version is required."
   exit 3
+fi
+
+if [ -z "$smallscripts_version" ]; then
+    echo "Error: --smallscripts-version is required."
+    exit 4
 fi
 
 if [ -z "$sequenceid" ]; then
   echo "Error: --sequenceid is required."
-  exit 4
-fi
-
-if [ -z "$config" ]; then
-  echo "Error: default --config is required."
   exit 5
 fi
 
@@ -80,26 +81,29 @@ fi
 cp ${samplesheet} ./
 
 # Clone repos, setup and activate envs
-echo "git clone --branch $pipeline_version $git_repo_url_tm"
-git clone --branch $pipeline_version $git_repo_url_tm
+echo "git clone --branch $pipeline_version $git_repo_url_tm" && \
+git clone --branch $pipeline_version $git_repo_url_tm && \
+git clone --branch $smallscripts_version $git_repo_url_smallscripts && \
 
-python3.11 -m venv venv_tm;
-source venv_tm/bin/activate;
-pip install -r pomfrey/requirements.txt;
-module load slurm-drmaa/1.1.3
-
+python3.11 -m venv venv_tm;  && \
+source venv_tm/bin/activate;  && \
+pip install -r pomfrey/requirements.txt;  && \
+module load slurm-drmaa/1.1.3 && \
 
 # Merge fastqs samples per lane into same file
-mkdir -p fastqs/
+mkdir -p fastqs/ && \
 for i in $(cat ${samplesheet} | grep -i "wp2_tm_" |awk -F "," '{print $1}' ); do 
-  $(echo $(basename ${perlane}/${i}_S*L001_R1_001.fastq.gz ) |cut -d '_' -f1-2); # If not L001 anymore, change
+  $(echo $(basename ${inbox_path}/fastq-perLane/${i}_S*L001_R1_001.fastq.gz ) |cut -d '_' -f1-2); # If not L001 anymore, change
   echo ${sample}
   cat ${inbox_path}/fastq-perLane/${i}_S*R1_001.fastq.gz >fastqs/${sample}_R1_001.fastq.gz && \
   cat ${inbox_path}/fastq-perLane/${i}_S*R2_001.fastq.gz >fastqs/${sample}_R2_001.fastq.gz
 done && \
 
 # Create config-file
-python3.9 /projects/wp2/nobackup/WP2_smallscripts/pipeline-starting-scripts/Pomfrey_Twist_myeloid/set_up_config.py -i ${config} -s ${sequenceid} && \
+config=WP2_smallscripts/pipeline-starting-scripts/Pomfrey_Twist_Myeloid/config_defaults_latest.yaml && \
+python3.9 WP2_smallscripts/pipeline-starting-scripts/Pomfrey_Twist_myeloid/set_up_config.py -i ${config} -s ${sequenceid} && \
+# Cp samplesheet to scratch
+cp ${samplesheet} . && \
 
 # Run pomfrey-snakemake line
 snakemake -p -j 120 --restart-times 1 --drmaa " --nodes=1-1 -A wp2 -p core -t {cluster.time} -n {cluster.n} " \
