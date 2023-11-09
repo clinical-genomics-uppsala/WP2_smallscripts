3e2908b86f6d90ea4d5a0d081d3e4e6b16939ad5
Arielle R Munters <arielle@reivant.se>
2023-11-09 15:44:34 +0100

    feat: add abl starting scripts and config

--- pipeline-starting-scripts/start_wp2_abl.sh
+++ pipeline-starting-scripts/start_wp2_abl.sh
@@ -4,12 +4,13 @@ set -e
 # cwd is scratch
 
 git_repo_url="https://github.com/clinical-genomics-uppsala/pickett_bcr_abl_pipeline.git"
+git_repo_url_smallscripts="https://github.com/clinical-genomics-uppsala/WP2_smallscripts.git"
 
 # Initialize variables
 inbox_path=""
 analysis_path=""
 pipeline_version=""
-snakemake_profile="" #"/projects/wp2/nobackup/WP2_smallscripts/snakemake-profiles/bcr_abl/"
+smallscripts_version=""
 
 # Process options and arguments
 while [[ $# -gt 0 ]]; do
@@ -26,9 +27,9 @@ while [[ $# -gt 0 ]]; do
             pipeline_version="$2"
             shift 2
             ;;
-	--profile_path)
-            snakemake_profile="$2"
-	    shift 2
+	    --smallscripts-version)
+            smallscripts_version="$2"
+	        shift 2
             ;;
         *)
             echo "Error: Unknown option or missing argument: $1"
@@ -53,32 +54,36 @@ if [ -z "$pipeline_version" ]; then
     exit 3
 fi
 
-if [ -z "$profile_path" ]; then
-    echo "Error: --profile-path is required."
+if [ -z "$smallscripts_version" ]; then
+    echo "Error: --smallscripts-version is required."
     exit 4
 fi
 
-cd $analysis_path
+cd $analysis_path # Behovs ej ar cwd?
 
 # Build env and activate it
 echo "clone repo, build and activate env" && \
 git clone --branch $pipeline_version $git_repo_url && \
+git clone --branch $smallscripts_version $git_repo_url_smallscripts && \
+
 python3 -m venv pickett_venv && \
 source pickett_venv/bin/activate && \
 pip install -r pickett_bcr_abl_pipeline/requirements.txt && \
 
 # Prep data
 echo "use hydra to build samples and units"  && \
-hydra-genetics create-input-files -d fastq/ -p MiSeq \
+hydra-genetics create-input-files -d ${inbox_path}/fastq/ -p MiSeq \
             -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA,AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -t R -s "(R[0-9]{2}-[0-9]{5})" -b NNNNNNNN && \
 sed -i 's/\t000000000-/\t/' units.tsv && \
-cp ${snakemake_profile}/marvin_config/*.yaml ./ && \
+cp WP2_smallscripts/snakemake-profiles/pickett_bcr_abl/marvin_config/*.yaml ./ && \
 
+# Cp samplesheet to scratch
+cp ${inbox_path}/SampleSheet.csv . && \
 
 # Run snakemake pipeline
 echo "Load slurm-drmaa and run snakemake pipeline"
 module load slurm-drmaa/1.1.3 && \
-snakemake --profile ${snakemake_profile} --configfile config.yaml && \
+snakemake --profile WP2_smallscripts/snakemake-profiles/pickett_bcr_abl/ --configfile config.yaml && \
 
 # Cp to inbox?
 echo "Cp Results to inbox"
