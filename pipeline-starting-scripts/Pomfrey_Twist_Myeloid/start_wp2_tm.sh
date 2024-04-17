18d90d8b723285664ab8cba71fb7a92d90a1b6e5
Arielle R Munters <arielle@reivant.se>
2024-04-17 10:21:52 +0200

    chore: include merging of fastq in set_up_config.py

--- pipeline-starting-scripts/Pomfrey_Twist_Myeloid/start_wp2_tm.sh
+++ pipeline-starting-scripts/Pomfrey_Twist_Myeloid/start_wp2_tm.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/env bash
 
-set -e
+set -euxo pipefail
+
+echo "RUNNING: wp2 tm"
 # cwd is scratch
 git_repo_url_tm="https://github.com/clinical-genomics-uppsala/pomfrey.git"
 git_repo_url_smallscripts="https://github.com/clinical-genomics-uppsala/WP2_smallscripts.git"
 
 # Initialize variables
+bin_path="/projects/bin/wp2_tm/"
 inbox_path=""
-analysis_path=""
-pipeline_version=""
+analysis_path=$(pwd)
+pomfrey_version=""
 smallscripts_version=""
 sequenceid=""
 samplesheet=""
 
 # Process options and arguments
 while [[ $# -gt 0 ]]; do
-  case "$1" in
+    case "$1" in
+    --bin-path)
+        bin_path="$2"
+        shift 2
+        ;;
     --inbox-path)
         inbox_path="$2"
         shift 2
         ;;
-    --analysis-path)
-        analysis_path="$2"
+    --pomfrey-version)
+        pomfrey_version="$2"
         shift 2
-        ;;
-    --pipeline-version)
-        pipeline_version="$2"
-        shift 2 
         ;;
     --smallscripts-version)
         smallscripts_version="$2"
@@ -36,15 +39,11 @@ while [[ $# -gt 0 ]]; do
         sequenceid="$2"
         shift 2
         ;;
-    --samplesheet)
-        samplesheet="$2"
-        shift 2
-        ;;
     *)
         echo "Error: Unknown option or missing argument: $1"
         exit 1
         ;;
-  esac
+    esac
 done
 
 # Check if required options are provided
@@ -53,14 +52,9 @@ if [ -z "$inbox_path" ]; then
     exit 1
 fi
 
-if [ -z "$analysis_path" ]; then
-    echo "Error: --analysis-path is required."
-    exit 2
-fi
-
-if [ -z "$pipeline_version" ]; then
-  echo "Error: --pipeline-version is required."
-  exit 3
+if [ -z "$pomfrey_version" ]; then
+    echo "Error: --pomfrey-version is required."
+    exit 3
 fi
 
 if [ -z "$smallscripts_version" ]; then
@@ -68,52 +62,52 @@ if [ -z "$smallscripts_version" ]; then
     exit 4
 fi
 
-# If no sequence id is defined, set it to current folder name
+# If no sequence-id is defined, set it to current folder name
 if [ -z "$sequenceid" ]; then
     sequenceid=${PWD##*/}
 fi
 
-# If no samplesheet defined, set samplesheet to inbox samplesheet, and cp to cwd
-if [ -z "$samplesheet" ]; then
-  samplesheet=$(echo ${inbox_path}"/SampleSheet.csv")
+pomfrey_path=${bin_path}/pomfrey/${pomfrey_version}/ &&
+    smallscripts_path=${bin_path}/wp2_smallscripts/${smallscripts_version}/ &&
+
+    # If correct pomfrey version not avail locally download and configure
+    if [ ! -d ${pomfrey_path} ]; then
+        echo "New version of Pomfrey needed; ${pomfrey_version}. Cloning repo, setup and activating env" &&
+            mkdir -p ${pomfrey_path} &&
+            cd ${pomfrey_path} &&
+            git clone --branch $pomfrey_version $git_repo_url_tm &&
+            echo "Cloning Pomfrey done, creating and activating env" &&
+            python3.9 -m venv venv_tm &&
+            source venv_tm/bin/activate &&
+            pip install -r pomfrey/requirements.txt &&
+            cd ${analysis_path}
+    else
+        source ${pomfrey_path}/venv_tm/bin/activate
+    fi
+
+# If correct version of WP2_smallscripts not avail locally, clone from github
+if [ ! -d ${smallscripts_path} ]; then
+    echo "New version of smallscripts needed; ${smallscripts_version}. Cloning repo" &&
+        mkdir -p ${smallscripts_path} &&
+        cd ${smallscripts_path} &&
+        git clone --branch $smallscripts_version $git_repo_url_smallscripts &&
+        cd ${analysis_path}
 fi
 
-# Clone repos, setup and activate envs
-echo "git clone --branch $pipeline_version $git_repo_url_tm & git clone --branch $smallscripts_version $git_repo_url_smallscript " && \
-git clone --branch $pipeline_version $git_repo_url_tm && \
-git clone --branch $smallscripts_version $git_repo_url_smallscripts && \
+module load slurm-drmaa/1.1.3 &&
+    echo "Set up config and start pipeline" &&
+    # Set up config and merge fastqs samples per lane into same file
+    mkdir -p fastq_merged/ &&
+    config=${smallscripts_path}/WP2_smallscripts/pipeline-starting-scripts/Pomfrey_Twist_Myeloid/config_defaults_latest.yaml &&
+    python3.9 ${smallscripts_path}/WP2_smallscripts/pipeline-starting-scripts/Pomfrey_Twist_Myeloid/set_up_config.py -i ${config} -s ${sequenceid} -fi ${inbox_path}/fastq/ &&
 
-python3.9 -m venv venv_tm;  && \
-source venv_tm/bin/activate;  && \
-pip install -r pomfrey/requirements.txt;  && \
-module load slurm-drmaa/1.1.3 && \
-
-# Merge fastqs samples per lane into same file
-mkdir -p fastqs/ && \
-for sample in $(cat ${samplesheet} | grep -i "wp2_tm_" |awk -F "," '{print $1}' ); do 
-  echo ${sample}
-  cat ${inbox_path}/fastq-perLane/${sample}_S*R1_001.fastq.gz >fastqs/${sample}_R1_001.fastq.gz && \
-  cat ${inbox_path}/fastq-perLane/${sample}_S*R2_001.fastq.gz >fastqs/${sample}_R2_001.fastq.gz
-done && \
-
-# Create config-file
-cp ${samplesheet} SampleSheet.csv && \
-config=WP2_smallscripts/pipeline-starting-scripts/Pomfrey_Twist_Myeloid/config_defaults_latest.yaml && \
-python3.9 WP2_smallscripts/pipeline-starting-scripts/Pomfrey_Twist_Myeloid/set_up_config.py -i ${config} -s ${sequenceid} && \
-
-
-# Run pomfrey-snakemake line
-snakemake -p -j 120 --restart-times 1 --drmaa " --nodes=1-1 -A wp2 -p core -t {cluster.time} -n {cluster.n} " \
--s pomfrey/src/somaticPipeline.smk \
---configfile ${sequenceid}_config.yaml \
---use-singularity  --singularity-prefix /projects/wp4/nobackup/singularity_cache/ \
---singularity-args "--cleanenv --bind /data --bind /projects " --latency-wait 5 \
---cluster-config pomfrey/cluster-config-uppsala.json \
---rerun-incomplete \
-&& \
-# Remove files?
-rm slurm-* && \
-#rm -r ${inbox_path}/fastq-perLane && \
-# Cp to inbox?
-rsync -ru Results ${inbox_path}/ && \
-rsync -ru variantCalls ${inbox_path}/ 
+    # Run pomfrey-snakemake line
+    snakemake -p -j 120 --restart-times 1 --drmaa " --nodes=1-1 -A wp2 -p core -t {cluster.time} -n {cluster.n} " \
+        -s ${pomfrey_path}/pomfrey/src/somaticPipeline.smk \
+        --configfile ${sequenceid}_config.yaml \
+        --use-singularity --singularity-prefix /projects/wp4/nobackup/singularity_cache/ \
+        --singularity-args "--cleanenv --bind /data --bind /projects " --latency-wait 5 \
+        --cluster-config ${pomfrey_path}/pomfrey/cluster-config-uppsala.json \
+        --rerun-incomplete \
+        --keep-going &&
+    rm slurm-*
