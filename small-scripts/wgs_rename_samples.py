1f29fb738855a1a1696b826d1ddb5ab884b5d152
Arielle R Munters <arielle@reivant.se>
2024-05-07 10:39:07 +0200

    feat: update renaming script to match bioinf samplesheet

--- small-scripts/wgs_rename_samples.py
+++ small-scripts/wgs_rename_samples.py
@@ -1,113 +1,128 @@
 #!/bin/python3
 
+import argparse
 import sys
 import itertools
-
-samples_file = sys.argv[1]
-units_file = sys.argv[2]
-samplesheet_file = sys.argv[3]
-
-header_row = "Sample_ID,Sample_Name,Description,I7_Index_ID,index,I5_Index_ID,index2,Sample_Project\n"
-header_row_split = header_row.strip().split(",")
+import shutil
 
 
-def test_samplesheet_line(samplesheet_line):
-    # length of Description
-    if len(samplesheet_line[2].split("_")) != 5:
-        sys.exit("Description field not 5 types for "+"\t".join(samplesheet_line))
-    # type
-    cell_type = samplesheet_line[2].split("_")[0]
-    if (cell_type.lower() != "t" and cell_type.lower() != "n" and cell_type.lower() != "heltranskriptom"
-            and cell_type.lower() != "r"):
-        sys.exit("Cell type is neither T|N|R|Heltranskriptom for sample " + samplesheet_line[0])
-    # sex
-    sex = line[2].split("_")[1]
-    if sex.upper() != "M" and sex.upper() != "K" and sex.upper() != "O" and sex.upper() != "NA":
-        sys.exit("Sex is neither M|K|O for sample " + samplesheet_line[0])
-    return True
+class DefaultList(list):
+    def __copy__(self):
+        return []
 
 
-samples = {}
-header = False
-with open(samplesheet_file, 'r') as samplesheet:
+# Initilize parser
+msg = "Script to rename wgs samples based on bioinformatic samplesheet"
+parser = argparse.ArgumentParser(description=msg, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+# Adding arguments
+parser.add_argument(
+    "-i", "--input", help="Bioinformatics samplesheet defining workpackages, experiment, analysis etc.", required=True
+)
+parser.add_argument("-o", "--output", help="Output ending to be added to old samples and units files", default="_old")
+parser.add_argument("--samples", help="Input samples.tsv file created with hydra genetics", default="samples.tsv")
+parser.add_argument("--units", help="Input units.tsv file created with hydra genetics", default="units.tsv")
+parser.add_argument(
+    "-w",
+    "--workpackage",
+    action="append",
+    help="Workpackages to include in output, one flag per workpackage e.g. -w wp2 -w wp3",
+    default=DefaultList(["wp2"]),
+)
+parser.add_argument(
+    "-a",
+    "--analysis",
+    action="append",
+    help="Analysis to include, one flag per analysis, e.g. -a hg -a tm",
+    default=DefaultList(["hg"]),
+)
+
+args = parser.parse_args()
+
+args.workpackage = [x.lower() for x in args.workpackage]
+args.analysis = [x.lower() for x in args.analysis]
+
+# Cp original samples.tsv och units.tsv to keep original
+shutil.copyfile(args.samples, args.samples + args.output)
+shutil.copyfile(args.units, args.units + args.output)
+
+
+# Parse bioinfo samplesheet
+samplesheet_dict = {}
+with open(args.input, "r") as samplesheet:
+    header_line = samplesheet.readline().lower().strip().split(",")
     for lline in samplesheet:
-        if len(lline.split()) == 0:  # skip blank lines
-            continue
-        if header:
-            line = lline.strip().split(",")
-            if line[7] == "WGSWP2" and test_samplesheet_line(line):
-                samples[line[0]] = {
-                                        header_row_split[1]: line[1],
-                                        "Type": line[2].split("_")[0],
-                                        "Sex": line[2].split("_")[1],
-                                        "Pedegree_id": line[2].split("_")[2],
-                                        "Seq_run": line[2].split("_")[3],
-                                        "Project": line[2].split("_")[4],
-                                        header_row_split[3]: line[3],
-                                        header_row_split[4]: line[4],
-                                        header_row_split[5]: line[5],
-                                        header_row_split[6]: line[6],
-                                        header_row_split[7]: line[7],
-                                    }
-        if lline == header_row:
-            header = True
+        line = lline.strip().lower().split(",")
+        if line[header_line.index("workpackage")] in args.workpackage and line[header_line.index("analysis")] in args.analysis:
+            samplesheet_dict[line[header_line.index("sample_id")]] = {}
+            i = 0
+            for column in header_line:
+                if column == "description":
+                    i += 1
+                    for description_field in line[header_line.index("description")].split("%"):
+                        samplesheet_dict[line[header_line.index("sample_id")]][description_field.split(":")[0]] = (
+                            description_field.split(":")[1]
+                        )
+                else:
+                    samplesheet_dict[line[header_line.index("sample_id")]][header_line[i]] = line[i]
+                    i += 1
+
+if len(samplesheet_dict) == 0:
+    raise Exception(
+        "No samples with workpackage" + ", ".join(args.workpackage) + " and analysis " + ", ".join(args.analysis) + " found."
+    )
 
 
-if len(samples) == 0:
-    raise Exception("No samples found, has the header in SampleSheet changed?")
+# Check that pedegree id/trio does not have two or more sex nor project ids
+dup_check_dict = {}
+for sample_id, sample_info in samplesheet_dict.items():
+    if sample_info["trio"] not in dup_check_dict:
+        dup_check_dict[sample_info["trio"]] = {}
+        dup_check_dict[sample_info["trio"]]["sex"] = sample_info["sex"]
+        dup_check_dict[sample_info["trio"]]["cgu-project"] = sample_info["cgu-project"]
+    elif dup_check_dict[sample_info["trio"]]["sex"] != sample_info["sex"]:
+        sys.exit("Same sample have two different sex " + sample_info["trio"])
+    elif dup_check_dict[sample_info["trio"]]["cgu-project"] != sample_info["cgu-project"]:
+        sys.exit("Same sample have two different cgu projects " + sample_info["trio"])
 
 
-# Check that the same Pedegree_id do not have two or more different sex nor project ids
-ped_sex_dict = {}
-ped_project_dict = {}
-for sample_id, sample_info in samples.items():
-    if sample_info["Pedegree_id"] not in ped_sex_dict.keys():
-        ped_sex_dict[sample_info["Pedegree_id"]] = sample_info["Sex"]
-    elif ped_sex_dict[sample_info["Pedegree_id"]] != sample_info["Sex"]:
-        sys.exit("Same sample have two different sex " + sample_info["Pedegree_id"])
-
-    if sample_info["Pedegree_id"] not in ped_project_dict.keys():
-        ped_project_dict[sample_info["Pedegree_id"]] = sample_info["Project"]
-    elif ped_project_dict[sample_info["Pedegree_id"]] != sample_info["Project"]:
-        sys.exit("Same sample have two different projects " + sample_info["Pedegree_id"])
-
-
-with open(samples_file.split(".")[0] + "_ped.tsv", 'w+') as out_samples:
-    with open(samples_file, 'r') as samples_tsv:
-        header = True
+# samples.tsv file
+with open(args.samples, "w+") as outfile:
+    with open(args.samples + args.output, "r") as samples_tsv:
         outlines = []
+        header_line = samples_tsv.readline().strip() + "\tproject\tsex\n"
+        outfile.write(header_line)
         for lline in samples_tsv:
-            if header:
-                out_samples.write(lline.strip() + "\tproject\tsex\n")
-                header = False
-            else:
-                line = lline.strip().split("\t")
-                if line[0] in samples.keys():
-                    ped_name = samples[line[0]]["Pedegree_id"]
-                    tumor_content = line[1]
-                    project = samples[line[0]]["Project"]
-                    sex = samples[line[0]]["Sex"]
+            line = lline.strip().lower().split("\t")
+            if line[0] in samplesheet_dict.keys():
+                ped_name = samplesheet_dict[line[0]]["trio"]
+                tc = line[1]
+                project = samplesheet_dict[line[0]]["cgu-project"]
+                sex = samplesheet_dict[line[0]]["sex"]
+                if sex.lower() != "m" and sex.lower() != "k" and sex.lower() != "o":
+                    sys.exit("Sex is neither M|K|O for sample " + line[0])
 
-                    outlines.append([ped_name.upper(), tumor_content, project, sex])
+                outlines.append([ped_name.upper(), tc, project, sex.upper()])
 
+    # Remove duplicate rows
     outlines.sort()
     for outline in [outlines for outlines, _ in itertools.groupby(outlines)]:
-        out_samples.write("\t".join(outline)+"\n")
+        outfile.write("\t".join(outline) + "\n")
 
 
-with open(units_file.split(".")[0]+"_ped.tsv", "w+") as out_units:
-    with open(units_file, 'r') as units_tsv:
-        header = True
+# units.tsv file
+with open(args.units, "w+") as outfile:
+    with open(args.units + args.output, "r") as units_tsv:
+        outlines = []
+        header_line = units_tsv.readline()
+        outfile.write(header_line)
         for lline in units_tsv:
-            if header:
-                out_units.write(lline)
-                header = False
-            else:
-                line = lline.strip().split("\t")
-                if line[0] in samples.keys():
-                    ped_name = samples[line[0]]["Pedegree_id"]
-                    cell_type = samples[line[0]]["Type"]
-                    if cell_type.lower() == "heltranskriptom" or cell_type.lower() == "r":
-                        cell_type = "R"
-                    outline = [ped_name.upper(), cell_type.upper()]+line[2:]
-                    out_units.write("\t".join(outline)+"\n")
+            line = lline.strip().split("\t")
+            if line[0].lower() in samplesheet_dict.keys():  # tar bort prov icke matchande workpackage analysis
+                ped_name = samplesheet_dict[line[0].lower()]["trio"]
+                sample_type = samplesheet_dict[line[0].lower()]["fragestallning"]
+                if sample_type == "heltranskriptom":
+                    sample_type = "r"
+                if sample_type != "t" and sample_type != "n" and sample_type != "r":
+                    sys.exit("Sample type/fragestallning is neither t|n|r|heltranskriptom " + sample_type)
+                outline = [ped_name.upper(), sample_type.upper()] + line[2:]
+                outfile.write("\t".join(outline) + "\n")
