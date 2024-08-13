ca8eb439af90bfe9b3872a642973eb25e38d35c8
Arielle R Munters <45485665+elleira@users.noreply.github.com>
2024-08-13 10:47:24 +0200

    feat: decompress spring to fastqs with decompress_spring2fastq.sh

---
+++ small-scripts/decompress_spring2fastq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --partition core,core_bkup
+#SBATCH --ntasks=8
+
+## To decompress spring compressed files into fastqs again. Fastq will end up in same dir as springfile
+
+module load singularity #does not work with apptainer for some reason but using 3.11.0 it works
+spring_file_full=$1
+
+if [ "${spring_file_full:0:1}" = "/" ]; then
+        wd_dir=$(dirname $spring_file_full)
+else
+        wd_dir=$(pwd)"/"$(dirname $spring_file_full)
+fi
+
+last_dir=$(echo $wd_dir | rev | cut -f1 -d"/" | rev)
+spring_file=$(basename $spring_file_full)
+outbase=$(basename $spring_file_full .spring)
+
+echo "singularity exec --bind ${wd_dir}/ docker://hydragenetics/spring:1.0.1 spring -t 8 -d -g -i ${last_dir}/${spring_file} -o ${last_dir}/${outbase}_R1.fastq.gz ${last_dir}/${outbase}_R2.fastq.gz"
+
+singularity exec --bind ${wd_dir}/ docker://hydragenetics/spring:1.0.1 spring -t 8 -d -g -i ${last_dir}/${spring_file} -o ${last_dir}/${outbase}_R1.fastq.gz ${last_dir}/${outbase}_R2.fastq.gz
